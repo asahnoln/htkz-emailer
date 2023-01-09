c99a339d006c6abbe0e9596597696a235fac7d9e
Arthur Aslanyan <arthur.e.aslanyan@gmail.com>
2023-01-10 03:51:59 +0600

    Amplitude service
    - [ADDED] Amplitude analytics

---
+++ services/emailer/Amplitude.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace app\services\emailer;
+
+use app\services\emailer\interfaces\AnalyticsInterface;
+use yii\httpclient\Client;
+
+class Amplitude implements AnalyticsInterface
+{
+    public function __construct(private Client $client, private string $url, private string $key)
+    {
+    }
+
+    public function send(string $id): bool
+    {
+        $response = $this->client->post($this->url, [
+            'api_key' => $this->key,
+            'events' => [
+                [
+                    'device_id' => 'mail_'.$id,
+                    'event_properties' => [
+                        'type' => 'mailing',
+                        'mail_type' => 'single',
+                        'is_auto' => true,
+                    ],
+                ],
+            ],
+        ], [
+            'content-type' => 'application/json',
+            'accept' => '*/*',
+        ])->send();
+
+        return $response->isOk;
+    }
+}
---
+++ tests/unit/services/AmplitudeTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace services;
+
+use Codeception\Stub\Expected;
+use app\services\emailer\Amplitude;
+use app\services\emailer\interfaces\AnalyticsInterface;
+use yii\httpclient\Client;
+use yii\httpclient\Request;
+use yii\httpclient\Response;
+
+class AmplitudeTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    protected function _before(): void
+    {
+    }
+
+    protected function _after(): void
+    {
+    }
+
+    // tests
+    public function testSendToAmplitude(): void
+    {
+        $url = 'http://testurl.com';
+        $client =  $this->make(Client::class, [
+            'post' => Expected::once(function ($u, $data, $headers) use ($url) {
+                verify($u)->equals($url);
+                verify($data['api_key'])->equals('secretKey');
+                verify($data['events'])->equals([
+                    [
+                        'device_id' => 'mail_testAmplitudeId',
+                        'event_properties' => [
+                            'type' => 'mailing',
+                            'mail_type' => 'single',
+                            'is_auto' => true,
+                        ],
+                    ],
+                ]);
+                verify($headers['content-type'])->equals('application/json');
+                verify($headers['accept'])->equals('*/*');
+
+                return $this->make(Request::class, [
+                    'send' => Expected::once(function () {
+                        return $this->make(Response::class, [
+                            'data' => ['code' => 200],
+                            'getStatusCode' => 200,
+                        ]);
+                    }),
+                ]);
+            }),
+        ]);
+        $a = new Amplitude($client, $url, 'secretKey');
+        $result = $a->send('testAmplitudeId');
+
+        verify($a)->instanceOf(AnalyticsInterface::class);
+        verify($result)->true();
+    }
+    
+    public function testReturnFalseOnBadCode(): void
+    {
+        $url = 'http://testurl.com';
+        $client =  $this->make(Client::class, [
+            'post' => Expected::once(function ($u, $data, $headers) use ($url) {
+                return $this->make(Request::class, [
+                    'send' => Expected::once(function () {
+                        return $this->make(Response::class, [
+                            'data' => ['code' => 400],
+                            'getIsOk' => false,
+                        ]);
+                    }),
+                ]);
+            }),
+        ]);
+        $a = new Amplitude($client, $url, 'secretKey');
+        $result = $a->send('testAmplitudeId');
+
+        verify($result)->false();
+    }
+}
