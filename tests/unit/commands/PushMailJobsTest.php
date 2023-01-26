c6e523ae4cc646f61cd81781340e90fbc90249dd
Arthur Aslanyan <arthur.e.aslanyan@gmail.com>
2023-01-26 13:46:35 +0600

    Testing general command
    - [CHANGED] Running command in general

--- tests/unit/commands/PushMailJobsTest.php
+++ tests/unit/commands/PushMailJobsTest.php
@@ -31,49 +31,49 @@ class PushMailJobsTest extends \Codeception\Test\Unit
     public function testPushMailJosbToQueue(): void
     {
         // TODO: Fix config for this test (should use console.php), then we can test it simpler?
-        // $result = \Yii::$app->createControllerByID('mail')->run('push');
-        // verify($result)->equals(ExitCode::OK);
-        $client = $this->make(Client::class, [
-            'get' => function () {
-                return $this->make(Request::class, [
-                    'send' => function () {
-                        return $this->make(Response::class, [
-                            'data' => [
-                                'tours' => [
-                                    [
-                                        'hotel' => ['name' => 'good'],
-                                        'price' => ['forTour' => 1],
-                                    ],
-                                    [
-                                        'hotel' => ['name' => 'bad'],
-                                        'price' => ['forTour' => 10],
-                                    ],
-                                    [
-                                        'hotel' => ['name' => 'ugly'],
-                                        'price' => ['forTour' => 100],
-                                    ],
-                                ],
-                            ],
-                            'getStatusCode' => 200,
-                        ]);
-                    },
-                ]);
-            },
-        ]);
-        $mc = new MailController('', '');
-        $m = new MailerSpy();
-        $a = new AnalyticsStub();
-
-        $e = new Emailer($m, $a);
-        $msg = new Message();
-        $q = new DbQueueStore();
-        $o = new DbOffer($client, 'http://testurl.com', 'secret');
-        $a = new DbAudience();
-
-        $code = $mc->actionPush($e, $msg, $q, $a, $o);
-
-        verify($code)->isInt();
-        verify($code)->equals(ExitCode::OK);
+        $result = \Yii::$app->createControllerByID('mail')->run('push');
+        verify($result)->equals(ExitCode::OK);
+        // $client = $this->make(Client::class, [
+        //     'get' => function () {
+        //         return $this->make(Request::class, [
+        //             'send' => function () {
+        //                 return $this->make(Response::class, [
+        //                     'data' => [
+        //                         'tours' => [
+        //                             [
+        //                                 'hotel' => ['name' => 'good'],
+        //                                 'price' => ['forTour' => 1],
+        //                             ],
+        //                             [
+        //                                 'hotel' => ['name' => 'bad'],
+        //                                 'price' => ['forTour' => 10],
+        //                             ],
+        //                             [
+        //                                 'hotel' => ['name' => 'ugly'],
+        //                                 'price' => ['forTour' => 100],
+        //                             ],
+        //                         ],
+        //                     ],
+        //                     'getStatusCode' => 200,
+        //                 ]);
+        //             },
+        //         ]);
+        //     },
+        // ]);
+        // $mc = new MailController('', '');
+        // $m = new MailerSpy();
+        // $a = new AnalyticsStub();
+        //
+        // $e = new Emailer($m, $a);
+        // $msg = new Message();
+        // $q = new DbQueueStore();
+        // $o = new DbOffer($client, 'http://testurl.com', 'secret');
+        // $a = new DbAudience();
+        //
+        // $code = $mc->actionPush();
+        //
+        // verify($code)->isInt();
+        // verify($code)->equals(ExitCode::OK);
 
         // verify($m->sentMessages)->arrayCount(6);
     }
